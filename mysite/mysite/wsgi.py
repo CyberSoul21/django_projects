5189f1994ba9741a68402fa831724bf2fa36d4c2
Wilson Javier Almario <wjalmarior@unal.edu.co>
2022-04-03 18:53:18 +0000

    My first commit from pythonAnywhere

---
+++ mysite/mysite/wsgi.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+#application = get_wsgi_application()
+
+import os
+import sys
+
+path = os.path.expanduser('~/django_projects/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
---
+++ mysite/polls/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.http import Http404
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
+from django.shortcuts import get_object_or_404, render
+
+
+
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. 97bf6036 is the polls index.")
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+'''
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+'''
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
+#def detail(request, question_id):
+#    return HttpResponse("You're looking at question %s." % question_id)
+
+'''
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+'''
+
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def owner(request):
+       return HttpResponse("Hello, world. 27ac0ca8 is the polls index.")
+
+
+
+
